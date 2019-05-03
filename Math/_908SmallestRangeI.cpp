29b7a299ca8089faa0a2e4b0c3c217ae2036f20c
mapicccy <773216251@qq.com>
2019-05-03 22:54:31 +0800

    smallest range I
    Signed-off-by: mapicccy <773216251@qq.com>

---
+++ Math/_908SmallestRangeI.cpp
@@ -0,0 +1,28 @@
+//
+// Created by mapicccy on 2019/5/3.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class _908SmallestRangeI {
+public:
+    int smallestRangeI(vector<int>& A, int K) {
+        int mn = A[0], mx = A[0];
+        for(auto a : A) {
+            if (a < mn) mn = a;
+            if (a > mx) mx = a;
+        }
+        return max(0, mx - mn - 2 * K);
+    }
+};
+
+int main(int argc, char **argv)
+{
+    auto *item = new _908SmallestRangeI;
+    vector<int> A = {0, 10};
+    cout << "ret: " << item->smallestRangeI(A, 2);
+}
