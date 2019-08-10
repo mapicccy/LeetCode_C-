1df6cba7fd28bd1b6e93bc0a9d84f12cfab2ae70
mapicccy <773216251@qq.com>
2019-08-10 15:12:03 +0800

    signle number
    Signed-off-by: mapicccy <773216251@qq.com>

---
+++ _136SingleNumber.cpp
@@ -0,0 +1,33 @@
+//
+// Created by mapicccy on 2019/8/10.
+//
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class _136SingleNumber {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret = nums.at(0);
+        for (int i = 1; i < nums.size(); i++) {
+            ret ^= nums.at(i);
+        }
+
+        return ret;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    auto item = new _136SingleNumber();
+    vector<int> nums;
+    nums.push_back(4);
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(2);
+    int ret = item->singleNumber(nums);
+
+    cout << "ret is " << ret;
+}
